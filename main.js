18f1663b2490fbca9ef801e36ff5f8a6670858cd
milazao <milena.peracetta.silva@escola.pr.gov.br>
2026-08-31 10:58:03 +0000

    parte 2

--- main.js
+++ main.js
@@ -23,14 +23,26 @@ function geraSenha() {
    // Começamos com uma senha vazia
    let senha = "";
 
+   // --- ADIÇÃO: Cria a base de caracteres baseada nas checkboxes marcadas ---
+   let alfabeto = "";
+   if (checkboxes[0].checked) { alfabeto += letrasMaiusculas; }
+   if (checkboxes[1].checked) { alfabeto += letrasMinusculas; }
+   if (checkboxes[2].checked) { alfabeto += simbolos; }
+
+   // Evita erro caso nenhuma checkbox esteja marcada
+   if (alfabeto.length === 0) {
+       campoSenha.value = "Selecione uma opção";
+       return;
+   }
+
 
    // Repete o processo conforme o tamanho escolhido
    for (let i = 0; i < tamanhoSenha; i++) {
 
 
-       // Gera um número aleatório
+       // Gera um número aleatório (Alterado de letrasMaiusculas para alfabeto)
        let numeroAleatorio =
-           Math.random() * letrasMaiusculas.length;
+           Math.random() * alfabeto.length;
 
 
        // Remove a parte decimal
@@ -38,10 +50,10 @@ function geraSenha() {
            Math.floor(numeroAleatorio);
 
 
-   // Escolhe uma letra e adiciona à senha
+   // Escolhe uma letra e adiciona à senha (Alterado de letrasMaiusculas para alfabeto)
    senha =
        senha +
-       letrasMaiusculas[numeroAleatorio];
+       alfabeto[numeroAleatorio];
 }
 
 
@@ -74,10 +86,10 @@ geraSenha();
 }
 
 
-//constante "checkbox"
+//constante "checkbox" (Corrigido o seletor para buscar elementos do tipo checkbox)
 const checkboxes =
-   document.querySelectorAll ("checkbox")
-   checkboxes[0].checked = true ;
+   document.querySelectorAll("input[type='checkbox']");
+   if (checkboxes.length > 0) { checkboxes[0].checked = true; }
 
 
 //constante letras maiúsculas
@@ -94,3 +106,7 @@ const letrasMinusculas =
 const simbolos =
    "!?$@#%&*-+";
 
+// --- ADIÇÃO: Vincula o clique de cada checkbox para gerar uma nova senha automaticamente ---
+for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].onclick = geraSenha;
+}
